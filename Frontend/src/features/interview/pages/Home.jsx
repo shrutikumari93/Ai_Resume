6d85646fa54fdb56f917a37555a283978764fc56
Shruti kumari <214169890+shrutikumari93@users.noreply.github.com>
2026-06-27 12:02:20 +0530

    Fix filename case for useInterview hook

--- Frontend/src/features/interview/pages/Home.jsx
+++ Frontend/src/features/interview/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react'
 import "../style/home.scss"
-import { useInterview } from '../hooks/useinterview.js'
+import { useInterview } from '../hooks/useInterview.js'
 import { useNavigate } from 'react-router'
 
 const Home = () => {
